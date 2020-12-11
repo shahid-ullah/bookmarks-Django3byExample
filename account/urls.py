7aae2ed7d368ab29ac20170b54eabb97a9a9bf5a
shahid <shahid.ice.nstu@gmail.com>
2020-12-11 15:55:48 +0600

    Add register user

--- account/urls.py
+++ account/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     # post views
     # path('login/', views.user_login, name='login')
     path('', views.dashboard, name='dashboard'),
+    path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
