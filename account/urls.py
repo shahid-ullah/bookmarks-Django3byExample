d47e48dedac8a1362139fd9cfe676757c9e17e1b
shahid <shahid.ice.nstu@gmail.com>
2020-12-12 14:52:29 +0600

    Extend user model with onetoone field and add edit user profile

--- account/urls.py
+++ account/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     # path('login/', views.user_login, name='login')
     path('', views.dashboard, name='dashboard'),
     path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 import debug_toolbar
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -22,3 +24,7 @@ urlpatterns = [
     path('account/', include('account.urls')),
     path('debug/', include(debug_toolbar.urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+            document_root=settings.MEDIA_ROOT)
